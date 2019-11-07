afd8787f94d5008eebfd0da19775d2fcc3d0b0f0
Shinta Koyanagi <yancya@upec.jp>
2019-11-07 18:11:05 +0900

    fetch with credentials

--- functions/functions/src/index.ts
+++ functions/functions/src/index.ts
@@ -15,7 +15,7 @@ admin.initializeApp(functions.config().firebase);
 const db = admin.firestore()
 
 export const accessCounter = functions.https.onRequest(async (request, response) => {
-  response.set('Access-Control-Allow-Origin', "*");
+  response.set('Access-Control-Allow-Origin', "https://yancya.house");
   const ip = request.ip;
   const ua = request.headers['user-agent'];
   const visitorAtToday = request.headers.cookie ?
